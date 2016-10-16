f37aeb6fb8b8309febdb251894ff844964a65888
dusong7 <dusong7@hotmail.com>
2016-10-16 22:59:21 +0800

    Test for convert function

---
+++ DS_learn/learn_alog/learn_alog/main.c
@@ -0,0 +1,42 @@
+//
+//  main.c
+//  learn_alog
+//
+//  Created by apple on 16/10/16.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#include <stdio.h>
+
+///number digital convert
+//
+void convert(char *s, int n , int b)
+{
+    char bit[] = {"0123456789ABCDEF"};
+    int len;
+    if (n == 0) {
+        strcpy(s,"");
+        return;
+    }
+    convert(s, n/b, b);
+    len = strlen(s);
+    s[len] = bit[n%b];
+    s[len+1] = '\0';
+}
+//////
+
+int main(int argc, const char * argv[]) {
+
+////Test for convert function/////
+//    char s[80];
+//
+//    int base = 2;
+//    int old = 10;
+//    convert(s, old, base);
+//
+//    printf("%s\n", s);
+/////////
+
+
+    return 0;
+}

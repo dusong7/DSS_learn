5b92bd88ecee221845c1dfc2ecd5d7c9dd6ea7b5
dusong7 <dusong7@hotmail.com>
2016-10-17 13:52:26 +0800

    Update main.c

--- DS_learn/learn_alog/learn_alog/main.c
+++ DS_learn/learn_alog/learn_alog/main.c
@@ -31,17 +31,31 @@ int a[MAXIN+1][MAXIN+1]  = {0};
 void gamecal(int N)
 {
     //
+    //
     int i = 0;
     int j = 0;
+    N -= 2;  // base people is 2, over 2
 
-    int nBase[2][2] = {1,2,2,1};
+    int nXBase = 1;
+    int nYBase = 2;
 
-    for(i=0; i<2; i++)
+    int nBase[2][2] = {0};
+
+    while(N >= 0)
     {
+        nBase[0][0] = nXBase + N;
+        nBase[0][1] = nYBase + N;
+        nBase[1][0] = nYBase + N;
+        nBase[1][1] = nXBase + N;
+
+        for(i=0; i<2; i++)
+     {
         for(j=0;j<2;j++)
         {
             printf("%d__", nBase[i][j]);
         }
+     }
+     N -=2;
     }
 }
 /////////////////////////////

4cfa4b12410c753e0c7992bd7ac69421a9338cb6
dusong7 <dusong7@hotmail.com>
2016-10-27 13:51:52 +0800

    up

--- DS_learn/single_link_list/single_link_list/sqlist.cpp
+++ DS_learn/single_link_list/single_link_list/sqlist.cpp
@@ -9,64 +9,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LINK_LIST_SIZE 10
-#define LISTINCREMENT 10
-#define OK 1
-#define OVERFLOW -1
-#define ERROR 0
+////////////Old execise/////////////////
+//#define LINK_LIST_SIZE 10
+//#define LISTINCREMENT 10
+//#define OK 1
+//#define OVERFLOW -1
+//#define ERROR 0
+//
+//
+//typedef int States;
+//typedef int Element;
+//
+//typedef struct  {
+//    Element *elem;
+//    int length;
+//    int listsize;
+//}Sqlist;
+//
+//States InitSqlit(Sqlist &L)
+//{
+//    L.elem = (Element *)malloc(LINK_LIST_SIZE * sizeof(Sqlist));
+//
+//    if (!L.elem) {
+//        return OVERFLOW;
+//    }
+//
+//    L.length = 0;
+//    L.listsize = LINK_LIST_SIZE;
+//
+//    return OK;
+//}
+//
+//void listInsert(Sqlist &L,  int position, Element e)
+//{
+////    int j;
+////    Element *newdb;
+//
+//    *(L.elem+position-1) = e;
+//    L.length ++;
+//}
+//
+//void list_traverse(Sqlist &L)
+//{
+//    for (int i = 0; i<L.length; i++) {
+//        printf("%d\n", *(L.elem));
+//    }
+//}
+//
+//    // insert code here...
+//
+//    Sqlist list;
+//    InitSqlit(list);
+//    listInsert(list, 1, 10);
+//    listInsert(list, 2, 4);
+//    listInsert(list, 3, 8);
+//
+//    list_traverse(list);
+//
+//    printf("Hello, World!\n");
+///////////////////////////////////////
 
+///new exercise_ Chapter 2///
+ int main(int argc, const char * argv[])
+ {
 
-typedef int States;
-typedef int Element;
-
-typedef struct  {
-    Element *elem;
-    int length;
-    int listsize;
-}Sqlist;
-
-States InitSqlit(Sqlist &L)
-{
-    L.elem = (Element *)malloc(LINK_LIST_SIZE * sizeof(Sqlist));
-
-    if (!L.elem) {
-        return OVERFLOW;
-    }
-
-    L.length = 0;
-    L.listsize = LINK_LIST_SIZE;
-
-    return OK;
-}
-
-void listInsert(Sqlist &L,  int position, Element e)
-{
-//    int j;
-//    Element *newdb;
-
-    *(L.elem+position-1) = e;
-    L.length ++;
-}
-
-void list_traverse(Sqlist &L)
-{
-    for (int i = 0; i<L.length; i++) {
-        printf("%d\n", *(L.elem));
-    }
-}
-
-int main(int argc, const char * argv[])
-{
-    // insert code here...
-
-    Sqlist list;
-    InitSqlit(list);
-    listInsert(list, 1, 10);
-    listInsert(list, 2, 4);
-    listInsert(list, 3, 8);
-
-    list_traverse(list);
-
-    printf("Hello, World!\n");
     return 0;
-}
+ }

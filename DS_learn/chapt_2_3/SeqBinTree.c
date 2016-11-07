7ac9e3f3fdf1edaa97f391e8059818c24ce0a5b0
dusong7 <dusong7@hotmail.com>
2016-11-07 17:01:17 +0800

    Update SeqBinTree.c

--- DS_learn/chapt_2_3/SeqBinTree.c
+++ DS_learn/chapt_2_3/SeqBinTree.c
@@ -7,3 +7,45 @@
 //
 
 #include "SeqBinTree.h"
+// ConsoleBinTree.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+
+#define MAX 100
+typedef int DATA;
+
+typedef struct ChainTree{
+	DATA data;
+	struct ChainTree *left;
+	struct ChainTree *right;
+	//struct ChainTree *parent;
+	
+}ChainBinTree;
+
+ChainBinTree *root = NULL;
+
+ChainBinTree *BinTreeInit(ChainBinTree *node)
+{
+	if (node != NULL)
+	{
+		return node;
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	ChainBinTree *bintree;
+	bintree = (ChainBinTree *)malloc(sizeof(ChainBinTree));
+	bintree->data = 10;
+	bintree->left = NULL;
+	bintree->right = NULL;
+	BinTreeInit(bintree);
+
+	return 0;
+}

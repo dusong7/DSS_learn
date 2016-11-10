031f5b52ec122897c84cbabd4775b4bb288e6bd9
dusong7 <dusong7@hotmail.com>
2016-11-10 15:27:44 +0800

    Create te2.cpp

---
+++ te2.cpp
@@ -0,0 +1,139 @@
+// ConsoleBinTree.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+
+#define MAX 100
+typedef int DATA;
+
+typedef struct ChainTree{
+	DATA data;
+	struct ChainTree *left;
+	struct ChainTree *right;
+	//struct ChainTree *parent;
+	
+}ChainBinTree;
+
+ChainBinTree *root = NULL;
+
+ChainBinTree *BinTreeInit(ChainBinTree *node)
+{
+	if (node != NULL)
+	{
+		return node;
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+ChainBinTree *InitRoot()
+{
+	ChainBinTree *node;
+	if (node = (ChainBinTree *)malloc(sizeof(ChainBinTree)))
+	{
+		node->data = 100;
+		node->left = NULL;
+		node->right = NULL;
+		return node;
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+int BinTreeAddNode(ChainBinTree *bt, ChainBinTree *node, int n)
+{
+	if (bt == NULL)
+	{ 
+		printf("Parent is NULL \n");
+	}
+
+	switch (n)
+	{
+	case 1:
+		if (bt->left)
+		{
+			printf("Left isnot NULL\n");
+		}
+		else
+		{
+			bt->left = node;
+		}
+		break;
+	case 2:
+		if (bt->right)
+		{
+			printf("Right isnot NULL\n");
+		}
+		else
+		{
+			bt->right = node;
+		}
+		break;
+	default:
+		printf("Param Error\n");
+		return 0;
+	}
+	return 1;
+}
+
+ChainBinTree *BinTreeLeft(ChainBinTree *bt)
+{
+	if (bt)
+	{
+		return bt->left;
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+ChainBinTree *BinTreeRight(ChainBinTree *bt)
+{
+	if (bt)
+	{
+		return bt->right;
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+void AddNode(ChainBinTree *bt)
+{
+	ChainBinTree *node, *parent;
+	DATA data;
+	char select;
+
+	if (node = (ChainBinTree *)malloc(sizeof(ChainBinTree)))
+	{
+		node->data = 101;
+		node->left = NULL;
+		node->right = NULL;
+
+		BinTreeAddNode(bt, node, 1);
+
+		node->data = 102;
+		BinTreeAddNode(bt, node, 2);
+	}
+
+}
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	ChainBinTree *root = NULL;
+	root = InitRoot();
+	AddNode(root);
+
+	ChainBinTree *left = BinTreeLeft(root);
+
+	return 0;
+}

835cc838e1ac4f19bb46a12d6959f05c4cdd8018
dusong7 <dusong7@hotmail.com>
2016-11-18 16:57:43 +0800

    Update Graph_.cpp

--- Graph_.cpp
+++ Graph_.cpp
@@ -1,1 +1,14 @@
-typedef
+#define VERTUX_MAX 26
+#define MAXVALUE 32767
+typedef struct
+{
+  char VERTEX[VERTUX_MAX];   //Weight value
+  int  Edges[VERTUX_MAX][VERTUX_MAX]; //
+  int  isTravel[VERTUX_MAX];  // 
+  int  VertexNum;
+  int  EdgeNum;  
+  int  GraphType;   //0 is undirected, 1 is directed
+}MatrixGraph;
+
+void CreateMatrix(MatrixGraph *G);  //Generate adjacancy matrix
+void OutMatrix(MatrixGraph *G);     //Out adjacency matrix

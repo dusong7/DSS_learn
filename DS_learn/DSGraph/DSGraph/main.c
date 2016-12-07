211cfad7111a0866557afcfabcd612f18127e188
dusong7 <dusong7@hotmail.com>
2016-12-07 21:42:30 +0800

    up

---
+++ DS_learn/DSGraph/DSGraph/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  DSGraph
+//
+//  Created by apple on 16/11/20.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    //
+    //
+    return 0;
+}
--- file_.cpp
+++ file_.cpp
@@ -24,3 +24,57 @@ int _tmain(int argc, _TCHAR* argv[])
 	return 0;
 }
 
+
+
+//
+//  main.c
+//  DSGraph
+//
+//  Created by apple on 16/11/20.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+//###############  x==x is FALSE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+
+    float x = 0xffffffff;
+
+    if (x == x) {
+        printf("Equal\n");
+    }
+    else {
+        printf("Not equal\n");
+    }
+
+    if (x >= 0) {
+        printf("x(%f) >= 0\n", x);
+    }
+    else if (x < 0) {
+        printf("x(%f) < 0\n", x);
+    }
+
+    int a = 0xffffffff;
+    memcpy(&x, &a, sizeof(x));
+    if (x == x) {
+        printf("Equal\n");
+    }
+    else {
+        printf("Not equal\n");
+    }
+
+    if (x >= 0) {
+        printf("x(%f) >= 0\n", x);
+    }
+    else if (x < 0) {
+        printf("x(%f) < 0\n", x);
+    }
+    else {
+        printf("Surprise x(%f)!!!\n", x);
+    }
+    return 0;
+}
+

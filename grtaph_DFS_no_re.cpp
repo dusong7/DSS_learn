2280fa2cf40887f94d58ac18407190d7ee111e20
dusong7 <dusong7@hotmail.com>
2016-12-08 11:58:08 +0800

    Update grtaph_DFS_no_re.cpp

--- grtaph_DFS_no_re.cpp
+++ grtaph_DFS_no_re.cpp
@@ -130,3 +130,121 @@ int main(void)
 
 	return 0;
 }
+
+////
+// LogTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <time.h>
+#include <Windows.h>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+stack<int> stcVal;
+
+//int main()
+//{
+//	stack<int> stcVal;
+//	
+//	for (size_t i = 0; i < 10; i++)
+//	{
+//		stcVal.push(i*i);
+//	}
+//	printf("%d",stcVal.top());
+//
+//    return 0;
+//}
+
+#define N 6  
+
+struct adj_matrix {
+	int vertex[N];
+	int edge[N][N];
+};
+
+struct adj_matrix matrix = {
+	{ 1, 2, 3, 4, 5, 6 },
+
+	{ { 0, 1, 0, 1, 0, 0 },
+	{ 0, 0, 0, 0, 1, 0 },
+	{ 0, 0, 0, 0, 1, 1 },
+	{ 0, 1, 0, 0, 0, 0 },
+	{ 0, 0, 0, 1, 0, 0 },
+	{ 0, 0, 0, 0, 0, 1 } }
+};
+
+int visit[N] = { 0, 0, 0, 0, 0, 0 };
+
+void dfs(struct adj_matrix *G)
+{
+	int i, j;
+	int x;
+
+	for (i = 0; i < N; i++) {
+		if (visit[i] != 1) {
+			visit[i] = 1;
+			printf("%3d", G->vertex[i]);
+			stcVal.push(i);
+
+			while (!stcVal.empty()) {
+				x = stcVal.top();
+				for (j = 0; j < N; j++) {
+					if (G->edge[x][j] == 1 && visit[j] != 1) {
+						visit[j] = 1;
+						printf("->%d", G->vertex[j]);
+						stcVal.push(j);
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+
+void maxTop(stack<int> &s)
+{
+	if (s.empty())
+	{
+		return;
+	}
+
+	int top1 = s.top();
+	s.pop();
+	if (!s.empty())
+	{
+		maxTop(s);
+		int top2 = s.top();
+		if (top1<top2)
+		{
+			s.pop();
+			s.push(top1);
+			s.push(top2);
+			return;
+		}
+	}
+	s.push(top1);
+}
+
+int main(void)
+{
+	//dfs(&matrix);
+	printf("\n");
+	stack<int> stckIntV;
+	int randomTable[] = { 1, 6, 4, 2, 3, 8, 7, 5};
+	for (size_t i = 0; i < 8; i++)
+	{
+		stckIntV.push(randomTable[i]);
+		maxTop(stckIntV);
+		int num = stckIntV.top();
+		printf("C_top%d\n", num);
+	}
+
+	for (size_t i = 0; i < 8; i++)
+	{
+		//
+	}
+	return 0;
+}

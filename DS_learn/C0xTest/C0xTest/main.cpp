47dd8a82e2d948e26024e6fc22d2b042fc9378a9
dusong7 <dusong7@hotmail.com>
2016-12-09 20:18:43 +0800

    up

--- DS_learn/C0xTest/C0xTest/main.cpp
+++ DS_learn/C0xTest/C0xTest/main.cpp
@@ -1,19 +1,15 @@
 //
-//  main.c
-//  DSGraph
+//  main.cpp
+//  C0xTest
 //
-//  Created by apple on 16/11/20.
+//  Created by apple on 16/12/9.
 //  Copyright © 2016年 apple. All rights reserved.
 //
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    //
-    //
-    
+    std::cout << "Hello, World!\n";
     return 0;
 }

9555616c2b3e8ccf55789dd853f468c7d5d7446f
dusong7 <dusong7@hotmail.com>
2016-12-18 19:28:24 +0800

    up

---
+++ D_learn2/Alo_Review1/Alo_Review1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Alo_Review1
+//
+//  Created by apple on 16/12/18.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

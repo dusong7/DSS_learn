27f1593aeaa9103c7f9e403355130fad3d99b410
dusong7 <dusong7@hotmail.com>
2016-12-25 17:39:10 +0800

    up

---
+++ DS_learn/TeCpp0x/TeCpp0x/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  TeCpp0x
+//
+//  Created by apple on 16/12/25.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//    char cResult[1000];
+//
+//    int nResult = 1;
+//    for (int i=1; i<24; i++) {
+//        nResult = i * nResult;
+//    }
+//
+//    cout<<nResult;
+}

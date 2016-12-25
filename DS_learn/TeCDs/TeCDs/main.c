8508144099434866ff519b8c0702651494a0805e
dusong7 <dusong7@hotmail.com>
2016-12-25 22:24:01 +0800

    u

---
+++ DS_learn/TeCDs/TeCDs/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  TeCDs
+//
+//  Created by apple on 16/12/25.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#include <stdio.h>
+#include <assert.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int i;
+    i = 0;
+    assert(++i);
+
+    printf("%d", i);
+
+    return 0;
+}
---
+++ ReviewCode/bra.cpp
@@ -0,0 +1,69 @@
+#include <iostram>
+#include <stack>
+
+using namespace std;
+
+bool isLeft(char c)
+{
+	if(c =="(")
+	{
+		return true;
+	}
+	
+	if(c == "{")
+	{
+		return true;
+	}
+}
+
+bool isMatched(char right, char left)
+{
+	if(right == "{" && left == "}")
+	{
+		return true;
+	}
+	
+	if(right == "(" &&left == ")")
+	{
+		return true;
+	}
+}
+
+bool BracketMatch(char *str)
+{
+	char c;
+	stack<char> stcChar;
+	
+	while(*str)
+	{
+		c = *str;
+		if(isLeft(c))
+		{
+			stcChar.push(c);
+		}
+		else
+		{
+			if(stcChar.empty() || !isMatched(c, stcChar.top()))
+			{
+				return false;
+			}
+			stcChar.pop();
+		}
+		str++;
+	}
+	
+	if(!stcChar.empty())
+	{
+		return false;
+	}
+	return true;
+}
+
+int main()
+{
+	char str[10000];
+	cin>>str;
+	cout<<BracketMatch(str);
+	
+	return 0;
+}

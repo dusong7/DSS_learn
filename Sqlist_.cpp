e3ffcbe611dcfb76aa676a08b1c88fc38505495c
dusong7 <dusong7@hotmail.com>
2017-02-03 16:58:05 +0800

    Update Sqlist_.cpp

--- Sqlist_.cpp
+++ Sqlist_.cpp
@@ -680,9 +680,8 @@ typedef struct {
 
 //implement LocateElem_SL
 int LocateElem_SL(SLinkList S, ElemType e, int(*compare)(ElemType, ElemType)) {
-	//在链表中查找第一个值为 e 的元素
-	//找到返回位置，否则返回 0
-	//compare 为比较函数
+	//find first value is e, yes return location,else return 0
+
 	int i = S[0].cur;
 	while (i && !compare(S[i].data, e))i = S[i].cur;
 	return i;
@@ -690,8 +689,7 @@ int LocateElem_SL(SLinkList S, ElemType e, int(*compare)(ElemType, ElemType)) {
 
  //implement InitSpace_SL
 void InitSpace_SL(SLinkList *space) {
-	//将一维数组中各各分量链成一个备用链表,space[0].cur为头指针
-	// "0" 表示空
+	//make array ,spare list, space[0],cur is head pointer
 	//space = (SLinkList*)malloc(sizeof(SLinkList));
 
 	int i;
@@ -703,8 +701,7 @@ void InitSpace_SL(SLinkList *space) {
 // implement Malloc_SL
 
 int Malloc_SL(SLinkList *space) {
-	//若备用链表不为空，则返回分配的结点下标，否则返回0
-
+    //if spare list not null, return node subscrip else return 0
 
 	int i = (*space)[0].cur;
 	if ((*space)[0].cur)
@@ -714,15 +711,14 @@ int Malloc_SL(SLinkList *space) {
 
 // implement Free_SL
 void Free_SL(SLinkList *space, int i) {/**<  */
-									   //将下标为i的空间作为备用空间
+	//make subscribe is i 's space as spare space
 	(*space)[i].cur = (*space)[0].cur;
 	(*space)[0].cur = i;
 }//Free_SL
 
 //implement difference
 void difference(SLinkList *space, int *S, int(*compare)(ElemType, ElemType)) {
-	//依次输入集合A和集合B的元素，在一维数组space中建立表示集合（A-B）U (B-A)的静态链表,S为其头指针，假设备用空间足够大,space[0].cur为其头指针
-	//InputData为输入函数
+	//A-B）U (B-A), space[0].cur is head pointer
 	int m, n, i, j, r, p, k;
 	ElemType b;
 	InitSpace_SL(space);
@@ -730,36 +726,36 @@ void difference(SLinkList *space, int *S, int(*compare)(ElemType, ElemType)) {
 	printf("Input m and n of A and B number:");
 	scanf("%d,%d", &m, &n);
 	printf("\nA:\n");
-	for (j = 1; j <= m; ++j) {//输入集合A
-		i = Malloc_SL(space);//分配结点
+	for (j = 1; j <= m; ++j) {//input set A
+		i = Malloc_SL(space);//alloc node
 							 //InputData(space[i].data);
 		scanf("%d", &(*space)[i].data);
-		(*space)[r].cur = i;//在末尾插入
+		(*space)[r].cur = i;//tail insert
 		r = i;
 	}//for
-	(*space)[r].cur = 0;//将结尾指针点设为空
+	(*space)[r].cur = 0;//set tail pointer empty
 
 	printf("\nB:\n");
-	for (j = 1; j <= n; ++j) {//输入集合B
-							  //InputData(b);//输入元素b
+	for (j = 1; j <= n; ++j) {//input set B
+							  //InputData(b);//input elem b
 		scanf("%d", &b);
 
 		p = *S;
-		k = (*space)[*S].cur;//K指向A中的第一个结点
-		while (k != (*space)[r].cur && !compare((*space)[k].data, b)) {//在当前链表中查找
+		k = (*space)[*S].cur;//K point A first node
+		while (k != (*space)[r].cur && !compare((*space)[k].data, b)) {//serach
 			p = k;
 			k = (*space)[k].cur;
 		}//while
-		if (k == (*space)[r].cur) {//当元素不存在时，将元素插入到r所指结点之后,且r位置不改变
+		if (k == (*space)[r].cur) {//if elem no exist, insert after r point, r position not change
 			i = Malloc_SL(space);
 			(*space)[i].data = b;
 			(*space)[i].cur = (*space)[r].cur;
 			(*space)[r].cur = i;
 		}
-		else {//if 当该元素存在时，将其删除
+		else {//if not exist , delete
 			(*space)[p].cur = (*space)[k].cur;
 			Free_SL(space, k);
-			if (r == k)r = p;//若将r删除，那么需要修改尾指针
+			if (r == k)r = p;//delete ,modify tails
 		}//else
 	}//for
 }//difference

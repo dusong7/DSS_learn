eefc1d17b93ccf291f20aa10d9b7acb1b747c10c
dusong7 <dusong7@hotmail.com>
2017-03-08 14:11:05 +0800

    t

---
+++ ReviewCode/testPolynomial.cpp
@@ -0,0 +1,12 @@
+// ConsoleTestPolynomial.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello\n");
+	return 0;
+}
+

8c8e162bdf1b2d6300978601ee806b69765969f7
dusong7 <dusong7@hotmail.com>
2017-03-09 10:49:00 +0800

    Update tePoly.cpp

--- ReviewCode/tePoly.cpp
+++ ReviewCode/tePoly.cpp
@@ -1,3 +1,8 @@
+// ContePoly.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
 #include <iostream>
 
 #define INFEASIBLE -1
@@ -11,22 +16,22 @@ typedef int Status;
 
 
 typedef struct {
-    float coef;
-    int expn;
-}term , ElemType;
+	float coef;
+	int expn;
+}term, ElemType;
 
 //typedef int ElemType;
 typedef int Status;
 
 typedef struct LNode
 {
-    ElemType data;
-    struct LNode *next;
+	ElemType data;
+	struct LNode *next;
 }*Link, *Position;
 
 typedef struct {
-    Link head, tail;
-    int len;
+	Link head, tail;
+	int len;
 }LinkList;
 
 typedef LinkList polynomail;
@@ -36,26 +41,26 @@ typedef LinkList polynomail;
 
 Status MakeNode(Link &p, ElemType e)
 {
-    p->data = e;
-    p->next = NULL;
+	p->data = e;
+	p->next = NULL;
 
-    return OK;
+	return OK;
 }
 
 ///Other way implement
 Position MakeNode_LL(ElemType e)
 {
-    //
-    Link p = NULL;
+	//
+	Link p = NULL;
 
-    p = (Link)malloc(sizeof(LNode));
-    if (!p)return ERROR;
-    else
-    {
-        p->data = e;
-        p->next = NULL;
-    }
-    return p;
+	p = (Link)malloc(sizeof(LNode));
+	if (!p)return ERROR;
+	else
+	{
+		p->data = e;
+		p->next = NULL;
+	}
+	return p;
 }
 
 //
@@ -63,28 +68,28 @@ Position MakeNode_LL(ElemType e)
 
 Status InitList_LL(LinkList &L)
 {
-    //
-    Link head = (Link)malloc(sizeof(LNode));
+	//
+	Link head = (Link)malloc(sizeof(LNode));
 
 
-    ElemType ep;
-    ep.expn = 1.0;
-    ep.coef = 1;
-//    MakeNode(head, ep);
-    L.head = MakeNode_LL(ep);
-    L.head = head;
+	ElemType ep;
+	ep.expn = 1;
+	ep.coef = 1.0;
+	MakeNode(head, ep);
+	//L.head = MakeNode_LL(ep);
+	L.head = head;
 
-    L.head->next = NULL;
-    L.tail = L.head;
-    L.len = 0;
-    return OK;
+	L.head->next = NULL;
+	L.tail = L.head;
+	L.len = 0;
+	return OK;
 }
 ///
 Position GetHead_LL(LinkList L)
 {
-    //Position p = (Position)malloc(sizeof(Position));
+	//Position p = (Position)malloc(sizeof(Position));
 
-    return L.head;
+	return L.head;
 }
 
 /// \param P
@@ -92,31 +97,31 @@ Position GetHead_LL(LinkList L)
 
 void CreadPoly(polynomail &P, int m)
 {
-    //
-    InitList_LL(P);
+	//
+	InitList_LL(P);
 
 }
 
 void DestroyPoly(polynomail &P)
 {
-//
+	//
 }
 
 void PrintPoly(polynomail &P)
 {
-//
+	//
 }
 
 void PolyLength(polynomail &P)
 {
-//
+	//
 }
 
 int main() {
-//    std::cout << "Hello, World!" << std::endl;
-    polynomail pl;
-    CreadPoly(pl, 1);
-    Link head = GetHead_LL(pl);
+	//    std::cout << "Hello, World!" << std::endl;
+	polynomail pl;
+	CreadPoly(pl, 1);
+	Link head = GetHead_LL(pl);
 
-    return 0;
+	return 0;
 }
